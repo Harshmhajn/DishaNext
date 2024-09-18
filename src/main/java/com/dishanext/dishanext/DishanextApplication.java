ce13aff5c9a616950f45b6cefb2d2a5890ddd192
Harsh Mahajan <premm7742@gmail.com>
2024-09-18 10:49:19 +0530

    first commit

---
+++ src/main/java/com/dishanext/dishanext/DishanextApplication.java
@@ -0,0 +1,13 @@
+package com.dishanext.dishanext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishanextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishanextApplication.class, args);
+	}
+
+}

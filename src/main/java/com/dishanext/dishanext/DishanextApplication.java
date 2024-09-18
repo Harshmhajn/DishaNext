c3191e7dfed0cf1535181b05a364afc4d097d278
Harsh Mahajan <premm7742@gmail.com>
2024-09-18 12:52:38 +0530

    first commit

--- src/main/java/com/dishanext/dishanext/DishanextApplication.java
+++ src/main/java/com/dishanext/dishanext/DishanextApplication.java
@@ -2,12 +2,20 @@ package com.dishanext.dishanext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DishanextApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DishanextApplication.class, args);
 	}
 
+	@GetMapping("/home")
+	@CrossOrigin
+	public String home(){
+		return "spring boot";
+	}
 }
